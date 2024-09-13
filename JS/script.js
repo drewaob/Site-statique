d0d2e6e307c69d8abd6ef371a36febdff9db7ee6
drewaob <andrewayoub95@gmail.com>
2024-09-13 12:10:01 +0200

    V6

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+// identifier le btn
+const btn = document.getElementById("btn")
+const st = document.getElementById("st")
+const main_section = document.querySelector(".main_section")
+// definir une function
+function handleClick(e) {
+	// e.target.innerText = "clicke le btn"
+	// selectioner le titre
+    st.innerText = "BOUTON CLIQUÉ  !!"
+    // rajout une classe
+	st.classList.add("red-text")
+
+	// changer son innerText
+}
+
+// rachouter une click event
+btn.addEventListener("click", handleClick)
+
+// log pour tester
+console.log("test ...", btn)
